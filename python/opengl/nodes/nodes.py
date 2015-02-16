19c8c6f9977060606a2e6d5038e229e029bc7af9
Mateusz Ploskon <ploskon@gmail.com>
2015-02-16 03:50:26 +0100

    openGL tests...

---
+++ python/opengl/nodes/nodes.py
@@ -0,0 +1,194 @@
+from OpenGL import GL
+from OpenGL import GLU
+from OpenGL import GLUT    
+import random
+import math
+
+class Node(object):
+    red       = 1.0, 0.0, 0.0, 0.3
+    green     = 0.0, 1.0, 0.0, 0.3
+    blue      = 0.0, 0.0, 1.0, 0.3
+    yellow    = 0.9, 0.9, 0.7, 0.3
+    spec_refl = 0.8, 0.8, 0.8, 1.0
+
+    def __init__(self, name=None, scale = 1.):
+        self.scale = scale
+        self.name  = name
+        self.translation = (0, 0, 0)
+        self.rotation = (0, 0, 0, 0)        
+        self.color = self.yellow
+        self.nodes = []
+        self.gLlist = None
+
+    def set_translation(self, x, y, z):
+        self.translation = (x, y, z)
+        
+    def set_rotation(self, a, x, y, z):
+        self.rotation = (a, x, y, z)
+
+    def set_color(self, color):
+        self.color = color
+
+    def add_node(self, n):
+        self.nodes.append(n)
+        self.build_list()
+        
+    def build_list(self):
+        #print self.name,'::_build_list scale = ',self.scale
+        #print self.name,'::_build_list gLlist = ',self.gLlist
+        if self.gLlist != None:
+            GL.glDeleteLists(self.gLlist, 1)
+        self.gLlist = GL.glGenLists(1)
+        GL.glNewList(self.gLlist, GL.GL_COMPILE)
+        #print self.name,'::_build_list translation',self.translation
+        self.glCodes()
+        GL.glEndList()
+        #print self.name,'::_build_list gLlist = ',self.gLlist
+            
+    def gl(self):
+        GL.glPushMatrix()
+        if self.gLlist == None:
+            self.build_list()            
+        ##print self.name,'::_gl gLlist = ',self.gLlist        
+        GL.glCallList(self.gLlist)
+        GL.glPopMatrix()
+
+    def glCodes(self):
+        GL.glPushMatrix()
+        GL.glTranslatef(self.translation[0], self.translation[1], self.translation[2])
+        GL.glRotatef(self.rotation[0], self.rotation[1], self.rotation[2], self.rotation[3])
+        self.glCode()
+        for n in self.nodes:
+            #print self.name,'::build_list',n.name,'::glCode'
+            n.glCodes()
+        GL.glPopMatrix()
+        
+    def glCode(self):
+        pass
+    
+class Coords(Node):
+    
+    def __init__(self, name=None, scale = 1.):
+        super(Coords, self).__init__(name, scale)
+        if self.name == None:
+            self.name = 'Coords'
+        
+    def glCode(self):
+        scale = 0.3
+        GL.glLineWidth(2.5); 
+        GL.glBegin(GL.GL_LINES)
+        
+        GL.glMaterialfv(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE, self.red)        
+        GL.glColor3f(1.0, 0.0, 0.0)
+        GL.glVertex3f(0.0, 0.0, 0.0)
+        GL.glVertex3f(scale * 1.0, 0.0, 0.0)
+        
+        GL.glMaterialfv(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE, self.green)        
+        GL.glColor3f(0.0, 1.0, 0.0)        
+        GL.glVertex3f(0.0, 0.0, 0.0)
+        GL.glVertex3f(0.0, scale * 1.0, 0.0)
+        
+        GL.glMaterialfv(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE, self.blue)        
+        GL.glColor3f(0.0, 0.0, 1.0)        
+        GL.glVertex3f(0.0, 0.0, 0.0)
+        GL.glVertex3f(0.0, 0.0, scale * 1.0)
+        GL.glEnd()
+
+class Cube(Node):
+    verts = [
+        ( 1.0,  1.0,  1.0),
+        ( 1.0, -1.0,  1.0),
+        (-1.0, -1.0,  1.0),
+        (-1.0,  1.0,  1.0)        
+    ]
+        
+    def __init__(self, name=None, scale = 1.):
+        super(Cube, self).__init__(name, scale)
+        if self.name == None:
+            self.name = 'Cube'
+
+    def square(self, rotation, color=None):
+        if color==None:
+            color = self.color
+        GL.glRotatef(rotation[0],rotation[1],rotation[2],rotation[3])
+        GL.glColor4f(0.3, 0, 0, 0.8)
+        GL.glMaterialfv(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE, color)
+        GL.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, self.spec_refl);               
+        GL.glBegin(GL.GL_QUADS)
+        for v in self.verts:
+            GL.glVertex3d(self.scale * v[0], self.scale * v[1], self.scale * v[2])        
+        GL.glEnd()
+        
+    def glCode(self):
+        #print self.name,'::glCode scale:', self.scale
+        self.square(rotation=(90,  0.0, 1.0, 0.0))
+        self.square(rotation=(180, 0.0, 1.0, 0.0))        
+        self.square(rotation=(90,  1.0, 0.0, 0.0))
+        self.square(rotation=(180, 1.0, 0.0, 0.0))        
+        self.square(rotation=(90,  0.0, 1.0, 0.0))
+        self.square(rotation=(180, 0.0, 1.0, 0.0))        
+
+class CoordCube(Cube):
+    def __init__(self, name=None, scale = 1.):
+        super(Cube, self).__init__(name, scale)
+        if self.name == None:
+            self.name = 'CoordCube'
+            
+    def glCode(self):
+        #print self.name,'::glCode scale:', self.scale
+        self.square(rotation=(90,  0.0, 1.0, 0.0), color=self.red    )
+        self.square(rotation=(180, 0.0, 1.0, 0.0), color=self.red    )        
+        self.square(rotation=(90,  1.0, 0.0, 0.0), color=self.green  )
+        self.square(rotation=(180, 1.0, 0.0, 0.0), color=self.green  )        
+        self.square(rotation=(90,  0.0, 1.0, 0.0), color=self.blue   )
+        self.square(rotation=(180, 0.0, 1.0, 0.0), color=self.blue   )
+        GL.glLineWidth(0.5);
+        GL.glMaterialfv(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE, self.yellow )
+        GLUT.glutWireCube(0.04)
+        
+class CoordCubes(Node):
+    def __init__(self, name=None, scale = 1.):
+        super(CoordCubes, self).__init__(name, scale)
+        if self.name == None:
+            self.name = 'CoordCubes'
+             
+        coords = Coords('coords')
+        self.nodes.append(coords)
+        
+        cube = CoordCube('cube',scale=0.015)
+        cube.set_translation(0,0,0)
+        self.nodes.append(cube)
+        
+        cubeX = CoordCube('cubeX',scale=0.01)
+        cubeX.set_translation(0.3,0,0)
+        self.nodes.append(cubeX)
+        
+        cubeY = CoordCube('cubeY',scale=0.01)
+        cubeY.set_translation(0,0.3,0)
+        self.nodes.append(cubeY)
+        
+        cubeZ = CoordCube('cubeZ',scale=0.01)
+        cubeZ.set_translation(0,0,0.3)
+        self.nodes.append(cubeZ)
+
+class RandomCubes(Node):
+    def __init__(self, name=None, scale = 1., randomd = 1.):
+        super(RandomCubes, self).__init__(name, scale)
+        if self.name == None:
+            self.name = 'RandomCubes'
+        self.randomd = randomd
+        for n in range(0, 1000):
+            for rn in range(1, 6):
+                cuber = Cube('cuber{}'.format(n),scale=0.005)
+                #r     = 1.-random.random()
+                r  = float(rn/10.) * 3.
+                rc = 1. - r/3.
+                theta = math.pi * 2. * random.random()            
+                z     = ( random.random() - 0.5 ) * 2.
+                p     = math.sqrt(1-z*z) * math.cos(theta), math.sqrt(1-z*z) * math.sin(theta), z
+                cuber.set_translation(r*p[0], r*p[1], r*p[2])
+                cuber.set_rotation(360*random.random(), random.random(), random.random(), random.random())
+                #cuber.set_color((random.random(), random.random(), random.random(), random.random()))
+                cuber.set_color((rc, rc, rc, rc))
+                self.nodes.append(cuber)
+
---
+++ python/opengl/nodes/test-nodes.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+
+# based on Rivebank's PyQt example
+
+import sys
+import math
+
+from PyQt5.QtCore import pyqtSignal, QPoint, QSize, Qt
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QVBoxLayout, QMessageBox, QSlider,
+        QWidget, QPushButton)
+from PyQt5.QtOpenGL import QGLWidget
+
+import random
+
+try:
+    from OpenGL import GL
+except ImportError:
+    app = QApplication(sys.argv)
+    QMessageBox.critical(None, "OpenGL hellogl",
+            "PyOpenGL must be installed to run this example.")
+    sys.exit(1)
+
+try:
+    from OpenGL import GLU
+except ImportError:
+    app = QApplication(sys.argv)
+    QMessageBox.critical(None, "OpenGL hellogl",
+            "GLU missing")
+    sys.exit(1)
+    
+try:
+    from OpenGL import GLUT    
+except ImportError:
+    app = QApplication(sys.argv)
+    QMessageBox.critical(None, "OpenGL hellogl",
+            "GLUT missing")
+    sys.exit(1)
+
+from nodes import *
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.glWidget = GLWidget()
+
+        self.xSlider = self.createSlider()
+        self.ySlider = self.createSlider()
+        self.zSlider = self.createSlider()
+
+        self.xSlider.valueChanged.connect(self.glWidget.setXRotation)
+        self.glWidget.xRotationChanged.connect(self.xSlider.setValue)
+        self.ySlider.valueChanged.connect(self.glWidget.setYRotation)
+        self.glWidget.yRotationChanged.connect(self.ySlider.setValue)
+        self.zSlider.valueChanged.connect(self.glWidget.setZRotation)
+        self.glWidget.zRotationChanged.connect(self.zSlider.setValue)
+
+        mainLayout = QHBoxLayout()
+        mainLayout.addWidget(self.glWidget)
+        mainLayout.addWidget(self.xSlider)
+        mainLayout.addWidget(self.ySlider)
+        mainLayout.addWidget(self.zSlider)
+        vLayout = QVBoxLayout()
+        vLayout.addLayout(mainLayout)
+        
+        self.resetButton = QPushButton("&Reset View", self)
+        self.resetButton.clicked.connect(self.resetView)
+        vLayout.addWidget(self.resetButton)
+        
+        self.quitButton = QPushButton("&Quit", self)
+        self.quitButton.clicked.connect(QApplication.instance().quit)
+        vLayout.addWidget(self.quitButton)
+        self.setLayout(vLayout)
+
+        self.xSlider.setValue(15 * 16)
+        self.ySlider.setValue(345 * 16)
+        self.zSlider.setValue(0 * 16)
+
+        self.setWindowTitle("Test Nodes")
+
+    def createSlider(self):
+        slider = QSlider(Qt.Vertical)
+
+        slider.setRange(0, 360 * 16)
+        slider.setSingleStep(16)
+        slider.setPageStep(15 * 16)
+        slider.setTickInterval(15 * 16)
+        slider.setTickPosition(QSlider.TicksRight)
+
+        return slider
+    
+    def resetView(self):
+        self.xSlider.setValue(0)
+        self.ySlider.setValue(0)
+        self.zSlider.setValue(0)
+    
+class GLWidget(QGLWidget):
+    xRotationChanged = pyqtSignal(int)
+    yRotationChanged = pyqtSignal(int)
+    zRotationChanged = pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super(GLWidget, self).__init__(parent)
+
+        self.object = 0
+        self.xRot = 0
+        self.yRot = 0
+        self.zRot = 0
+
+        self.lastPos = QPoint()
+        
+    #check this for light and color tracking:
+    # http://www.falloutsoftware.com/tutorials/gl/gl8.htm
+    def setupLight(self):
+        self.light_ambient   = 0.5, 0.5, 0.5, 0.5
+        self.light_diffuse   = 0.5, 0.5, 0.5, 0.5 # diffuse light.
+        self.light_specular  = 0.5, 0.5, 0.5, 0.5
+        self.light0_position = 0.0, 0.0, -20.0, 1.0
+        #GL.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT,  self.light_ambient);
+        #GL.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE,  self.light_diffuse)
+        #GL.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, self.light_specular);
+        #GL.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, self.light0_position)
+        #GL.glEnable(GL.GL_LIGHT0);
+        
+        self.global_ambient = 0.9, 0.9, 0.9, 1.0
+        GL.glLightModelfv(GL.GL_LIGHT_MODEL_AMBIENT, self.global_ambient);
+        
+        # Enable a single OpenGL light.
+        GL.glEnable(GL.GL_LIGHTING);
+        
+    def minimumSizeHint(self):
+        return QSize(50, 50)
+
+    def sizeHint(self):
+        return QSize(400, 400)
+
+    def setXRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.xRot:
+            self.xRot = angle
+            self.xRotationChanged.emit(angle)
+            self.updateGL()
+
+    def setYRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.yRot:
+            self.yRot = angle
+            self.yRotationChanged.emit(angle)
+            self.updateGL()
+
+    def setZRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.zRot:
+            self.zRot = angle
+            self.zRotationChanged.emit(angle)
+            self.updateGL()
+
+    def initializeGL(self):
+        GL.glClearColor(0,0,0,0)
+        self.setupLight()
+        
+        #GL.glShadeModel(GL.GL_FLAT)
+        GL.glShadeModel(GL.GL_SMOOTH)
+        GL.glEnable(GL.GL_DEPTH_TEST)
+        #GL.glEnable(GL.GL_CULL_FACE)
+
+        #/* Setup the view of the cube. */
+        GL.glMatrixMode(GL.GL_PROJECTION);
+        #GLU.gluPerspective( 90.0,   #/* field of view in degree */ 
+        #                    1.0,   #/* aspect ratio */ 
+        #                    1.0,   #/* Z near */ 
+        #                    10.0); #/* Z far */ 
+        GL.glMatrixMode(GL.GL_MODELVIEW);
+        #GLU.gluLookAt(0.0, 0.0, 10.0,  #/* eye is at (0,0,5) */
+        #              0.0, 0.0,  0.0,      #/* center is at (0,0,0) */
+        #              0.0, 0.0,  0.);      #/* up is in positive Y direction */
+  
+        #/* Adjust cube position to be asthetic angle. */
+        #GL.glTranslatef(0.0, 0.0, -1.0);
+        #GL.glRotatef(60, 1.0, 0.0, 0.0);
+        #GL.glRotatef(-20, 0.0, 0.0, 1.0);
+           
+        #make all the things...
+        self.objects = []
+        self.objects.append(CoordCubes())
+        if '--cubes' in sys.argv:
+            self.objects.append(RandomCubes())        
+        
+    def paintGL(self):
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
+        GL.glLoadIdentity()
+        GL.glTranslated(-0.5, 0.5, -10.0)
+        GL.glRotated(self.xRot / 16.0, 1.0, 0.0, 0.0)        
+        for o in self.objects:
+            o.gl()
+        GL.glLoadIdentity()            
+        GL.glTranslated(-0.5, -0.5, -10.0)
+        GL.glRotated(self.yRot / 16.0, 0.0, 1.0, 0.0)        
+        for o in self.objects:
+            o.gl()
+        GL.glLoadIdentity()            
+        GL.glTranslated( 0.5, -0.5, -10.0)
+        GL.glRotated(self.zRot / 16.0, 0.0, 0.0, 1.0)        
+        for o in self.objects:
+            o.gl()
+        GL.glLoadIdentity()
+        GL.glTranslated(0.0, 0.0, -10.0)
+        GL.glRotated(self.xRot / 16.0, 1.0, 0.0, 0.0)
+        GL.glRotated(self.yRot / 16.0, 0.0, 1.0, 0.0)
+        GL.glRotated(self.zRot / 16.0, 0.0, 0.0, 1.0)        
+        for o in self.objects:
+            o.gl()
+            
+    def resizeGL(self, width, height):
+        side = min(width, height)
+        aspect = float(width) / float(height)
+        if side < 0:
+            return
+
+        #GL.glViewport((width - side) // 2, (height - side) // 2, side, side)
+        GL.glViewport(0, 0, side, side)
+
+        GL.glMatrixMode(GL.GL_PROJECTION)
+        GL.glLoadIdentity()
+        #GL.glOrtho(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0)
+        GL.glOrtho(-1.0 * aspect, +1 * aspect, -1, +1, 4.0, 15.0)        
+        #GL.glOrtho(-0.5, +0.5, +0.5, -0.5, 0.0, 100.0)
+        #GL.glOrtho(-1, 1, 1, -1, 1.0, 100.0)        
+        #GL.glOrtho(0.0, width, height, 0.0, 0.0, 1.0);
+        #aspectRatio = float(width) / float(height);
+        #if width >= height:
+        #    GLU.gluOrtho2D(-0.5*aspectRatio, 0.5*aspectRatio, 0.0, 1.0); 
+        #else:
+        #    GLU.gluOrtho2D(-0.5, 0.5, 0.0, 1.0/aspectRatio);        
+        GL.glMatrixMode(GL.GL_MODELVIEW)
+
+    def mousePressEvent(self, event):
+        self.lastPos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        dx = event.x() - self.lastPos.x()
+        dy = event.y() - self.lastPos.y()
+
+        if event.buttons() & Qt.LeftButton:
+            self.setXRotation(self.xRot + 8 * dy)
+            self.setYRotation(self.yRot + 8 * dx)
+        elif event.buttons() & Qt.RightButton:
+            self.setXRotation(self.xRot + 8 * dy)
+            self.setZRotation(self.zRot + 8 * dx)
+
+        self.lastPos = event.pos()
+    
+    def normalizeAngle(self, angle):
+        while angle < 0:
+            angle += 360 * 16
+        while angle > 360 * 16:
+            angle -= 360 * 16
+        return angle
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
