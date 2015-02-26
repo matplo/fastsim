02cb7b05d3ccac8448d6842633c64511d1c7292c
matplo <ploskon@gmail.com>
2015-02-26 01:26:58 +0100

    seems to be working

--- python/dircrawl/cuts.py
+++ python/dircrawl/cuts.py
@@ -4,3 +4,76 @@
         print doc
         for key in doc.keys():
             print 'key is:', key, ' | object is:', doc[key]
+
+
+    def process_by_name(self):
+        t = self.pop_tag_named('Title:')
+        if t != None:
+            self.title = ''.join(t.content)
+        t = self.pop_tag_named('Subtitle:')
+        if t != None:
+            self.subtitle = ''.join(t.content)
+        t = self.pop_tag_named('Content:')
+        if t != None:
+            self.content = '\n'.join(t.content)
+
+
+def load_stream_to_docs(stream):
+    tag = None    
+    docs = []
+    current_doc = []
+    for l in stream:
+        for k in Config.KEYWORDS:
+            if k == l[:len(k)]:
+                if tag:
+                    current_doc.append(tag)
+                if k == '- ':
+                    key = 'Content:'
+                else:
+                    key = k
+                tag = Tag(key)
+        if tag:
+            if tag.key == '...':
+                docs.append(current_doc)
+                current_doc = []
+                tag = None
+                continue
+            if tag.key == l[:len(tag.key)]:
+                s = l[len(tag.key):].strip('\n')
+            else:
+                s = l.strip('\n')
+            if len(s) > 0:
+                tag.append(s)
+    if tag:
+        current_doc.append(tag)
+    if len(current_doc) > 0:
+        docs.append(current_doc)
+    return docs
+
+def load_file(fname):
+    with open(fname) as f:
+        stream = f.readlines()
+    #print stream
+    docs = load_stream_to_docs(stream)
+    #debug_obj(docs)
+    for i,doc in enumerate(docs):
+        #print i,doc
+        #for tag in doc:
+        #    print 'key is:', tag.key, ' | object is:', tag.content
+        print '---------'
+        s = Section(doc)
+        #s.process()
+        #print 'title:', s.pop_tag_key('Title:').content
+        #print 'sub:', s.pop_tag_key('Subtitle:').content        
+        #debug_obj(s)
+        #print i, s
+        print s.render()
+
+def main():
+    load_file('testinput.txt')
+    #s = Section(testin)
+    #print
+    #print s.render()
+    
+if __name__ == '__main__':
+    main()
--- python/dircrawl/section.py
+++ python/dircrawl/section.py
@@ -4,105 +4,138 @@ from debug_utils.debug_utils import *
 import jinja2
 import os
 
-class Section(object):
+class Config(object):
     KEYWORDS = ['---', 'Title:', 'Subtitle:', 'Content:', 'Raw:', 'File.txt:', '...', '- ']
-    def __init__(self, tags, template = 'section_template.html'):
-        self.template = template
-        self.tags     = tags
-        self.title    = 'not set'
-        self.subtitle = 'not set'
-        self.content  = 'not set'
-        self.process()
+    TEMPLATES = {
+        'Section'  : 'section_template.html',
+        'Raw:'     : 'section_element_raw.html',
+        'image'    : 'section_element_image.html',
+        'Content:' : 'section_element_standard.html'}
+    def __init__(self, path = os.path.dirname(__file__)):
+        self.path = path
         
-        self.jenv = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-                    
-    def render(self):
-        template = self.jenv.get_template(self.template)
-        tvalues = { 'section' : self }
-        return template.render(tvalues)
-
-    def pop_tag(self, name):
-        for i,t in enumerate(self.tags):
-            if t.key == name:
-                self.tags.pop(i)
-                return t
-        return None
-    
-    def process(self):
-        t = self.pop_tag('Title:')
-        if t != None:
-            self.title = ''.join(t.content)
-        t = self.pop_tag('Subtitle:')
-        if t != None:
-            self.subtitle = ''.join(t.content)
-        t = self.pop_tag('Content:')
-        if t != None:
-            self.content = '\n'.join(t.content)
-
-    def __repr__(self):
-        return '\n > '.join([self.title,self.subtitle,self.content])
-    
+    def set_path(self, path):
+        self.path = path
+        
+    def template(self, s):
+        return self.path + '/' + self.TEMPLATES[s]
+            
 class Tag(object):
     def __init__(self, key):
         self.key = key
         self.content = []
     def append(self, c):
         self.content.append(c)
+    def string(self):
+        return '\n'.join(self.content)
     def __repr__(self):
         return self.key
-    
-def load_stream_to_docs(stream):
-    tag = None    
-    docs = []
-    current_doc = []
-    for l in stream:
-        for k in Section.KEYWORDS:
-            if k == l[:len(k)]:
-                if tag:
-                    current_doc.append(tag)
-                if k == '- ':
-                    key == 'Contents:'
-                else:
-                    key = k
-                tag = Tag(key)
-        if tag:
-            if tag.key == '...':
-                docs.append(current_doc)
-                current_doc = []
-                tag = None
-                continue
-            if tag.key == l[:len(tag.key)]:
-                s = l[len(tag.key):].strip('\n')
-            else:
-                s = l.strip('\n')
-            if len(s) > 0:
-                tag.append(s)
-    if tag:
-        current_doc.append(tag)
-    if len(current_doc) > 0:
-        docs.append(current_doc)        
-    return docs
 
-def load_file(fname):
+class Section(object):
+    def __init__(self, tags, config = Config()):
+        self.tconfig  = config
+        self.tags     = tags
+        self.title    = 'not set'
+        self.subtitle = 'not set'
+        self.content  = 'not set'
+        self.jloader  = jinja2.FileSystemLoader(self.tconfig.path)
+        self.jenv     = jinja2.Environment(loader=self.jloader)
+        self.process()        
+
+    def pop_tag_key(self, keyname):
+        for i,t in enumerate(self.tags):
+            if t.key == keyname:
+                self.tags.pop(i)
+                return t
+        return Tag(keyname)
+    
+    def pop_tag(self):
+        if len(self.tags) > 0:
+            t = self.tags[0]
+            self.tags.pop(0)
+            return t
+        return None
+
+    def render_element(self, t):
+        tvalues = { 'content' : t.string() }
+        tmpfile = self.tconfig.template(t.key)
+        template = self.jenv.get_template(tmpfile)
+        retval = template.render(tvalues)
+        #print 'render ele:',t.key,retval
+        return retval
+    
+    def render(self):
+        tvalues = { 'section' : self }
+        template = self.jenv.get_template(self.tconfig.template('Section'))
+        return template.render(tvalues)
+    
+    def process(self):
+        self.title    = self.pop_tag_key('Title:').string()
+        self.subtitle = self.pop_tag_key('Subtitle:').string()
+        content = []
+        for t in self.tags:
+            if t.key == 'Content:':
+                content.append(self.render_element(t))
+            if t.key == 'Raw:':
+                content.append(self.render_element(t))
+        self.content = ''.join(content)
+        
+    def __repr__(self):
+        return '\n > '.join([self.title,self.subtitle,self.content])
+
+class File(object):
+    def __init__(self, stream, config=Config()):
+        self.load_stream_to_docs(stream)
+        self.config   = config
+        self.sections = []
+        for d in self.docs:
+            self.sections.append(Section(d, self.config))
+
+    def render(self):
+        summed = []
+        for s in self.sections:
+            summed.append(s.render())
+        return '\n'.join(summed)
+    
+    def load_stream_to_docs(self, stream):
+        tag = None    
+        self.docs = []
+        current_doc = []
+        for l in stream:
+            for k in Config.KEYWORDS:
+                if k == l[:len(k)]:
+                    if tag:
+                        current_doc.append(tag)
+                    if k == '- ':
+                        key = 'Content:'
+                    else:
+                        key = k
+                    tag = Tag(key)
+            if tag:
+                if tag.key == '...':
+                    self.docs.append(current_doc)
+                    current_doc = []
+                    tag = None
+                    continue
+                if tag.key == l[:len(tag.key)]:
+                    s = l[len(tag.key):].strip('\n')
+                else:
+                    s = l.strip('\n')
+                if len(s) > 0:
+                    tag.append(s)
+        if tag:
+            current_doc.append(tag)
+        if len(current_doc) > 0:
+            self.docs.append(current_doc)
+
+def from_file(fname):
     with open(fname) as f:
         stream = f.readlines()
-    #print stream
-    docs = load_stream_to_docs(stream)
-    #debug_obj(docs)
-    for i,doc in enumerate(docs):
-        #print i,doc
-        #for tag in doc:
-        #    print 'key is:', tag.key, ' | object is:', tag.content
-        print '---------'
-        s = Section(doc)        
-        #print i, s
-        print s.render()
-            
+    return File(stream)
+    
 def main():
-    load_file('testinput.txt')
-    #s = Section(testin)
-    #print
-    #print s.render()
+    fs = from_file('testinput.txt')
+    print fs.render()
     
 if __name__ == '__main__':
     main()
