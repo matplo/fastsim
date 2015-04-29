2a2b65c1a0b9af569f182c536fdaf136f3396a15
matplo <ploskon@gmail.com>
2015-04-29 23:49:35 +0200

    know were you are...

--- physics/hfpy8/setenv.sh
+++ physics/hfpy8/setenv.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
-alice
-. $HOME/devel/subjets/bin/setenv_all.sh
-. $HOME/devel/subjets/pythia8/setenv.sh
-export PYTHIA8LOCATION=$PYTHIA8DIR
+function exec_lbnlcern()
+{
+	alice
+	. $HOME/devel/subjets/bin/setenv_all.sh
+	. $HOME/devel/subjets/pythia8/setenv.sh
+	export PYTHIA8LOCATION=$PYTHIA8DIR
+}
+
+function exec_darwin()
+{
+	setroot
+	pythia8	
+}
+
+hostid=`uname -a`
+case $hostid in
+    *.nersc*)
+        [ "$NERSC_HOST" == "pdsf" ] && exec_pdsf
+        [ "$NERSC_HOST" == "carver" ] && exec_carver
+        hostn=`uname -n`
+        case $hostn in
+            pc*)
+                exec_pdsf
+                ;;
+        esac
+        ;;
+    *Darwin*)
+        exec_darwin
+        ;;
+    *lbnl?core*)
+        exec_lbnlcern
+        ;;
+    *)
+        exec_default
+        ;;
+esac
+
 
 
