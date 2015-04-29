647b089960d1a2abd79da6af106ce0331649d228
matplo <ploskon@gmail.com>
2015-04-30 00:23:28 +0200

    add a flag for send to background '--bg' - add & at the end of the command

--- physics/hfpy8/gen_multiple.py
+++ physics/hfpy8/gen_multiple.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
+import sys
+
 #pThatmins = [ 5, 10, 15, 20, 25, 30, 40, 50, 70 ]
 pThatmins = [ 5, 10, 15]
 quark     = [0, 1]
 nevents   = 50000
 
+inbg = ''
+if '--bg' in sys.argv:
+	inbg = '&'
+
 for q in quark:
     for b in pThatmins:
     	for onia in [0,1]:
     		logname = '{}-{}-{}-{}.log'.format(nevents, b, q, onia)
-    		print 'root -l -q \"gener.C({},{},{},{})\" > {}'.format(nevents, b, q, onia, logname)
+    		print 'root -l -q \"gener.C({},{},{},{})\" > {} {}'.format(nevents, b, q, onia, logname, inbg)
