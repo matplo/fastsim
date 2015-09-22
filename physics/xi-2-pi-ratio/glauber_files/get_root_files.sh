3c76d1e5e220f5ccb19467a3da2224b981d3bfe1
matplo <ploskon@gmail.com>
2015-09-22 02:16:47 +0200

    download to a root file

--- physics/xi-2-pi-ratio/glauber_files/get_root_files.sh
+++ physics/xi-2-pi-ratio/glauber_files/get_root_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget https://www.dropbox.com/s/uuzaweqbk32sh76/glau_ppb_ntuple.root?dl=0
-wget https://www.dropbox.com/s/vc5l53vc0kapnoi/glau_pbpb_ntuple.root?dl=0
+wget https://www.dropbox.com/s/uuzaweqbk32sh76/glau_ppb_ntuple.root?dl=0 -O glau_ppb_ntuple.root
+wget https://www.dropbox.com/s/vc5l53vc0kapnoi/glau_pbpb_ntuple.root?dl=0 -O glau_pbpb_ntuple.root
+
 
