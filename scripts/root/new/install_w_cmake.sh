a8e36e5ad90b81bfc4670bd0bac84315f19e7686
matplo <ploskon@gmail.com>
2015-10-07 00:53:42 +0200

    a bit smarter script - options configure, build, install

--- scripts/root/new/install_w_cmake.sh
+++ scripts/root/new/install_w_cmake.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+function is_arg_in_set
+{
+	#echo $@
+	last1="${@: -1}"
+	last2="${@:(-2):1}"
+	#echo "last 2nd is:" $last2
+	for i in $@ ; do
+	    if [[ $i == $last1 ]] ; then
+	    	return 0 # this is true
+	    fi
+		if [[ $i == $last2 ]]; then
+			break
+		fi
+	done
+	return 1 # this is false
+}
+
+args=$@
+function is_arg_set
+{
+	for i in $args ; do
+	    if [[ $i == $1 ]] ; then
+	    	return 0 #this is true
+	    fi
+	done
+	return 1 #this is false
+}
+
+build=false
+configure=false
+install=false
+
+if is_arg_set "configure"; then configure=true; fi
+if is_arg_set "build"; then build=true; configure=true; fi
+if is_arg_set "install"; then build=true; configure=true; install=true; fi
+
+
+echo "[i] configure:"$configure
+echo "[i] build:"$build
+echo "[i] install:"$install
+
 savedir=$PWD
 
 # from https://root.cern.ch/get-root-sources
@@ -18,13 +59,13 @@ version=v5-34-34
 tdir=$HOME/software/root/$version
 
 cd root
-git tag -l
+#git tag -l
 #git checkout -b $version 
 #$version
 
 git checkout tags/$version
 
-if [ "$1" = "build" ]; then
+if $configure; then
 
 	cd $wdir
 	
@@ -35,7 +76,15 @@ if [ "$1" = "build" ]; then
 	cd $bdir
 	
 	cmake $wdir/root
+fi
+
+if $build; then
+
 	cmake --build .
+fi
+
+if $install; then
+
 	cmake -DCMAKE_INSTALL_PREFIX=$tdir -P cmake_install.cmake
 	
 	ln -s $tdir/bin/thisroot.sh "/usr/local/bin/root-$version"
