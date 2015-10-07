0f65b4a26312f530101a92c40812c0cebafcbe57
Mateusz Ploskon <mateusz@lbnl3core.cern.ch>
2015-10-07 20:59:19 +0200

    few fixes but the module needs to be in path anyhow...

--- run2/trigger/test/test_toy.sh
+++ run2/trigger/test/test_toy.sh
@@ -1,15 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 module list
-module load root/v5-34-34 fastjet/3.1.2 hepmc/2.06.09 lhapdf/5.9.1 pythia/8205
+module load use.own
+module load hepsoft/latest 
+module load run2trigger
 module list
-echo $RUN2EMCTRIGGER
-export DYLD_LIBRARY_PATH=$RUN2EMCTRIGGER/lib:$DYLD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$RUN2EMCTRIGGER/lib:$LD_LIBRARY_PATH
-export PATH=$RUN2EMCTRIGGER/bin:$PATH
 
-echo "LD:"$LD_LIBRARY_PATH
-echo "DYLD:"$DYLD_LIBRARY_PATH
 cd $RUN2EMCTRIGGER/test
 ln -s $RUN2EMCTRIGGER/gener/config/toymcpy8.cmnd
 otool -L $RUN2EMCTRIGGER/bin/toymcpy8
