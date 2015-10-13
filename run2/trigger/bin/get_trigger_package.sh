4c40745701e9815f4e3f849d01a3cfbf975474c5
matplo <ploskon@gmail.com>
2015-10-13 17:36:11 +0200

    switch to Markus' git

--- run2/trigger/bin/get_trigger_package.sh
+++ run2/trigger/bin/get_trigger_package.sh
@@ -5,8 +5,8 @@ savedir=$PWD
 if [ -d $RUN2EMCTRIGGER ]; then
 	cd $RUN2EMCTRIGGER
 	[ "clean" == "$1" ] && rm -rfv ./EmcalTriggerFast
-	#git clone https://github.com/mfasDa/EmcalTriggerFast
-	git clone https://github.com/matplo/EmcalTriggerFast
+	git clone https://github.com/mfasDa/EmcalTriggerFast
+	#git clone https://github.com/matplo/EmcalTriggerFast
 fi
 
 cd $savedir
