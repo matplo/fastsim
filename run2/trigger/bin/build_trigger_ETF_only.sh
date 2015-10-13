eb796dd18259f73a15dc0c5d2a07a7f8fc3234e2
matplo <ploskon@gmail.com>
2015-10-13 02:22:42 +0200

    follow changes in build of ETF

--- run2/trigger/bin/build_trigger_ETF_only.sh
+++ run2/trigger/bin/build_trigger_ETF_only.sh
@@ -10,7 +10,7 @@ if [ -d $RUN2EMCTRIGGER ]; then
     [ "$1" == "realclean" ] && rm -rf $bdir
     mkdir -p $bdir
     cd $bdir
-    cmake -DCMAKE_INSTALL_PREFIX=$RUN2EMCTRIGGER $RUN2EMCTRIGGER/$pack/src
+    cmake -DCMAKE_INSTALL_PREFIX=$RUN2EMCTRIGGER $RUN2EMCTRIGGER/$pack
     [ "$1" == "clean" ] && make clean
     [ "$1" == "verbose" ] && verbose="VERBOSE=1"
     make $verbose && make install
--- run2/trigger/bin/build_trigger_package.sh
+++ run2/trigger/bin/build_trigger_package.sh
@@ -4,13 +4,13 @@ savedir=$PWD
 
 if [ -d $RUN2EMCTRIGGER ]; then
   cd $RUN2EMCTRIGGER
-  for pack in EmcalTriggerFast AliGenFME toymcpy8
+  for pack in EmcalTriggerFast AliGenFME/src toymcpy8/src
   do
     bdir=$RUN2EMCTRIGGER/.build/$pack
     [ "$1" == "realclean" ] && rm -rf $bdir
     mkdir -p $bdir
     cd $bdir
-    cmake -DCMAKE_INSTALL_PREFIX=$RUN2EMCTRIGGER $RUN2EMCTRIGGER/$pack/src
+    cmake -DCMAKE_INSTALL_PREFIX=$RUN2EMCTRIGGER $RUN2EMCTRIGGER/$pack
     [ "$1" == "clean" ] && make clean
     [ "$1" == "verbose" ] && verbose="VERBOSE=1"
     make $verbose && make install
