dc29a9726fad7355888e50c3a532821df9097345
matplo <ploskon@gmail.com>
2015-10-14 14:29:54 +0200

    the other energy

---
+++ run2/trigger/generate/2.76TeV/job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#. /u/startup/pdsf.profile
+module load use.own
+module load run2trigger
+module list
+
+#env
+date
+
+rundir=XXX
+mult=XXX
+
+cd $rundir
+
+pwd
+#$RUN2EMCTRIGGER/bin/toymcpy8 --nev 1000 --hardeta --emc --mult 1000. 2>&1 | tee run.log
+$RUN2EMCTRIGGER/bin/toymcpy8 --emc --mult $mult 2>&1 | tee run.log
+date
+
+cd -
+
