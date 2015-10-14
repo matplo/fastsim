c5511c3dcc4930649b9589c0e7d7fc26cfb1c51a
matplo <ploskon@gmail.com>
2015-10-15 00:34:06 +0200

    qsub on ubuntu

--- run2/trigger/generate/5TeV/make_job_scripts.sh
+++ run2/trigger/generate/5TeV/make_job_scripts.sh
@@ -33,8 +33,10 @@ function write_script
         newdir="./photons/mult-$4/bin-$1-$2"
     fi
     mkdir -p $newdir
-    echo "find . -name \"job.sh\" -exec {} \;" > $newdir/../run_all.sh
+    echo "find $PWD -name \"job.sh\" -exec {} \;" > $newdir/../run_all.sh
     chmod +x $newdir/../run_all.sh
+    echo "find $PWD -name \"submit.sh\" -exec {} \;" > $newdir/../submit_all.sh
+    chmod +x $newdir/../submit_all.sh
     cd $newdir
     spwd=$PWD
     cp -v $RUN2EMCTRIGGER/generate/5TeV/emctrig.cmnd $RUN2EMCTRIGGER/generate/5TeV/job.sh .
@@ -56,7 +58,8 @@ function write_script
 
     chmod +x ./job.sh
     echo "#!/bin/bash" > ./submit.sh
-    echo "qsub -P alice -o $PWD -e $PWD -m e -M mploskon@lbl.gov job.sh" >> ./submit.sh
+    #echo "qsub -P alice -o $PWD -e $PWD -m e -M mploskon@lbl.gov job.sh" >> ./submit.sh
+    echo "qsub -o $PWD -e $PWD $PWD/job.sh" >> ./submit.sh
     chmod +x ./submit.sh
     if is_arg_set "submit" ;
     then
