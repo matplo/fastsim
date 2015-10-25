573b0b85b1f68c74fce99c54b172ae27ec4e9d9c
matplo <ploskon@gmail.com>
2015-10-25 03:37:15 +0100

    R=0.2 and R=0.4 changed the --femc to 0.3

--- run2/trigger/generate/5TeV/job.sh
+++ run2/trigger/generate/5TeV/job.sh
@@ -20,10 +20,12 @@ pwd
 #$RUN2EMCTRIGGER/bin/toymcpy8 --emc --mult $mult 2>&1 | tee run.log
 #$RUN2EMCTRIGGER/bin/toymcpy8 --emc --mult $mult --femc 0.1 2>&1 | tee run.log
 
-radius=0.4
 radius=0.2
-$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --emc-par 2>&1 | tee run.log
-$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --emc-par --femc 0.1 2>&1 | tee run.log
+#$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --emc-par 2>&1 | tee run.log
+$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --emc-par --femc 0.3 2>&1 | tee run.log
+radius=0.4
+#$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --emc-par 2>&1 | tee run.log
+$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --emc-par --femc 0.3 2>&1 | tee run.log
 
 date
 
