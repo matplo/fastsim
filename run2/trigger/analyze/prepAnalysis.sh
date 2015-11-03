1b0cac5eafb5328d42f1b0240e714b6524031015
matplo <ploskon@gmail.com>
2015-11-03 04:06:36 +0100

    make histograms - other files

--- run2/trigger/analyze/prepAnalysis.sh
+++ run2/trigger/analyze/prepAnalysis.sh
@@ -3,6 +3,11 @@
 #./testAnalysis.py --dir /home/mateusz/devel/sandbox/run2/trigger/generate/5TeV/hardQCD/mult-12 --name qcd-mtune1.2
 #./testAnalysis.py --dir /home/mateusz/devel/sandbox/run2/trigger/generate/5TeV/photons/mult-12 --name photons-mtune1.2
 
-./testAnalysis.py --dir /home/mateusz/devel/sandbox/run2/trigger/generate/5TeV/hardQCD/mult-10 --name qcd-mtune1.0
-./testAnalysis.py --dir /home/mateusz/devel/sandbox/run2/trigger/generate/5TeV/photons/mult-10 --name photons-mtune1.0
+#./testAnalysis.py --dir /home/mateusz/devel/sandbox/run2/trigger/generate/5TeV/hardQCD/mult-10 --name qcd-mtune1.0
+#./testAnalysis.py --dir /home/mateusz/devel/sandbox/run2/trigger/generate/5TeV/photons/mult-10 --name photons-mtune1.0
+
+./testAnalysis.py --dir /home/mateusz/devel/sandbox/run2/trigger/generate/5TeV/hardQCD/mult-0.3 --name qcd-mtune1.0-femc0.3
+
+./testAnalysis.py --dir /home/mateusz/devel/sandbox/run2/trigger/generate/2.76TeV/hardQCD/mult-10 --name qcd-mtune1.0-2.76TeV
+
 
