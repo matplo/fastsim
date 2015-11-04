91280207504464aad783013f016c582a4c77775c
matplo <ploskon@gmail.com>
2015-11-04 09:55:20 +0100

    par fix

--- run2/trigger/analyze/prepAnalysis.py
+++ run2/trigger/analyze/prepAnalysis.py
@@ -84,7 +84,7 @@ def write_scripts(infiles=None):
     with open(subm_script, 'w') as fs:
         for sc in scripts:
             if '--pdsf' in sys.argv:
-                print >> fs, 'qsub -P alice -o {} -e {} -m e -M mploskon@lbl.gov {}'.format(os.path.dirname(sc), sc)
+                print >> fs, 'qsub -P alice -o {1} -e {1} -m e -M mploskon@lbl.gov {2}'.format(os.path.dirname(sc), sc)
             else:
                 print >> fs,'qsub -d',os.path.dirname(sc),sc
     print '[i] written:',subm_script
