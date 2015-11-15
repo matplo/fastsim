b0ac0c6c8105c09df81909d8d07cf9403ba6ce68
matplo <ploskon@gmail.com>
2015-11-16 00:07:38 +0100

    more util

---
+++ bash/util/thisdir.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function abspath()
+{
+  case "${1}" in
+    [./]*)
+    echo "$(cd ${1%/*}; pwd)/${1##*/}"
+    ;;
+    *)
+    echo "${PWD}/${1}"
+    ;;
+  esac
+}
+
+savedir=$PWD
+
+THISFILE=`abspath $BASH_SOURCE`
+XDIR=`dirname $THISFILE`
+if [ -L ${THISFILE} ];
+then
+    target=`readlink $THISFILE`
+    XDIR=`dirname $target`
+fi
+
+THISDIR=$XDIR
+echo $THISDIR
