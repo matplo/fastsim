2b98de2c689f94d90576595fdb3f5ad66f156606
matplo <ploskon@gmail.com>
2015-11-23 23:16:40 +0100

    default 1.2 GB - now fixed

--- run2/trigger/generate/5TeV/job.sh
+++ run2/trigger/generate/5TeV/job.sh
@@ -20,9 +20,9 @@ radius=0.4
 ## $RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --emc-par --realbg --femcpar 2>&1 | tee run.log
 ## $RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --mtune 1.0 --emc-par --realbg --femc 0.3 2>&1 | tee run.log
 ##$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --mtune 1.0 --emc-par --realbg --femcpar 2>&1 | tee run.log
-##$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --mtune 1.2 --emc-par --realbg --femcpar 2>&1 | tee run.log
+$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --mtune 1.2 --emc-par --realbg --femcpar 2>&1 | tee run.log
 
-$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --mtune 1.0 --emc-par --realbg --femc 1.2 2>&1 | tee run.log
+#$RUN2EMCTRIGGER/bin/toymcpy8 -R $radius --mtune 1.0 --emc-par --realbg --femc 1.0 2>&1 | tee run.log
 
 date
 
