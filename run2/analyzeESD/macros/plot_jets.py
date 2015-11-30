6fd1365ec8abbf6f489d270da7d8e877114367ca
matplo <ploskon@gmail.com>
2015-11-30 16:31:28 +0100

    bad channels handling

---
+++ run2/analyzeESD/macros/plot_jets.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import pyutils as ut
+import tutils as tu
+import draw_utils as du
+import dlist
+import ROOT
+import os
+import IPython
+import math
+import sys
+
+def get_jets(indir, var='', cuts='', bwidth=10, xlow= 0, xhigh=300):
+	print '[i] get_jets:',var
+	tu.getTempCanvas().cd()
+	tname = 't'
+	refcuts = None
+	tu.getTempCanvas().cd()
+	hl = draw_ntuple.h1d_from_ntuple_dir_filter(indir, tname, var, cuts, bwidth, xlow, xhigh, refcuts=refcuts, nev=-1, thr=5, fpatt='tree-*.root') #nev=10000, thr = 1 * bwidth/2.) #thr was 100!
+	return hl
+
+def main(dname):
+	hl = get_jets('./', 't.jets_CINT7.fE', '')
+
+if __name__ == '__main__':
+	tu.setup_basic_root()
+	dname = ut.get_arg_with('--in')
+	if dname==None:
+		dname = './'
+	main(dname)
+	if not ut.is_arg_set('-b'):
+		IPython.embed()	
---
+++ run2/analyzeESD/macros/unique_bad_channels.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import pyutils as ut
+import tutils as tu
+import draw_utils as du
+import dlist
+import ROOT
+import os
+import IPython
+import math
+import sys
+
+def reformat(chlist, fout=None):
+	print >> fout, 'Int_t AliAnalysisM::fgBadChannels[] = {'
+	for i, ich in enumerate(chlist):
+		if i % 10 == 0 and i!=0:
+			print >> fout
+		sich = '{}'.format(ich).zfill(5)
+		if i < len(chlist) - 1:
+			print >> fout,sich+',',
+	print >> fout, '-1};'
+
+def main(fname, foutname=None):
+	with open(fname) as f:
+		indata = f.readlines()
+	output = []
+	doubled = 0
+	for l in indata:
+		if '#' in l:
+			continue
+		channels = l.split(',')
+		for ch in channels:
+			ch = ch.strip().strip('\n')
+			if len(ch) < 1:
+				continue	
+			ich = int(ch)		
+			if ich <= 0:
+				continue
+			if ich in output:
+				doubled += 1
+			else:
+				#print ich
+				output.append(ich)
+	print '// doubled:',doubled
+	fout = sys.stdout
+	if foutname != None:
+		fout = open(foutname, 'w')
+	reformat(output, fout)
+	if foutname != None:
+		fout.close()
+
+if __name__ == '__main__':
+	tu.setup_basic_root()
+	fname = ut.get_arg_with('--in')
+	if fname==None:
+		fname = 'bad_channels.txt'
+	foutname = fname.replace('.txt', '_updated.txt')
+	main(fname, foutname)
+#	if not ut.is_arg_set('-b'):
+#		IPython.embed()	
