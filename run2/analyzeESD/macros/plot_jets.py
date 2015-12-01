f037e08486c03796096b9c7f2d8cc324aab8098c
matplo <ploskon@gmail.com>
2015-12-01 05:41:56 +0100

    plot jet spectrum etc

--- run2/analyzeESD/macros/plot_jets.py
+++ run2/analyzeESD/macros/plot_jets.py
@@ -12,31 +12,86 @@ import sys
 
 import draw_ntuple
 
-kOther=1
-kCINT7=2
-kEJ1=4
-kEG1=8
-kDJ1=16
-kDG1=32
+kOther = 1
+kCINT7 = 2
+kEJ1   = 4
+kEG1   = 8
+kDJ1   = 16
+kDG1   = 32
 
-def get_jets(indir, var='', cuts='', bwidth=10, xlow= 0, xhigh=300):
+def tbit(ttype):
+	d = dict([('kOther' , 1),('kCINT7' , 2),('kEJ1' , 4),('kEG1' , 8),('kDJ1' , 16), ('kDG1' , 33)])
+	return d[ttype]
+
+def get_jets(indir, var='', cuts='', bwidth=5, xlow= 0, xhigh=200):
 	print '[i] get_jets:',var
 	tu.getTempCanvas().cd()
 	ntname = 't'	
 	tu.getTempCanvas().cd()
 	hl = draw_ntuple.h1d_from_ntuple_dir(indir, ntname, var, cuts, bwidth, xlow, xhigh, 
 								title='h', modname='', nev=-1, fpatt='Tree_*.root')
-	return hl
+	return hl.sum()
+
+def get_n_events(indir, ttype, bwidth=100, xlow= 0, xhigh=200):
+	cuts = 'trig.type & {}'.format(ttype)
+	var =  'trig.type'
+	ntname = 't'	
+	tu.getTempCanvas().cd()
+	tu.getTempCanvas().cd()
+	hl = draw_ntuple.h1d_from_ntuple_dir(indir, ntname, var, cuts, bwidth, xlow, xhigh, 
+										title='h', modname='', nev=-1, fpatt='Tree_*.root')
+	nentries = hl.sum().obj.GetEntries()
+	print '[i] get_n_events:',var,cuts,nentries
+	return nentries
 
 def main(dname):
 	ls = dlist.ListStorage('jets')
 
-	hl = get_jets(dname, 'jets00.fE', '(trig.type && 4)')
-	ls.append(hl)
+	for fdet in [0, 1]:
+		for jets in [ 'jets00.fE', 'jets02.fE', 'jets04.fE' ]:
+			for ttype in ['kCINT7', 'kEJ1', 'kEG1', 'kDJ1', 'kDG1']:
+				bit = tbit(ttype)
+				if fdet == 0 and 'kD' in ttype: continue
+				if fdet == 1 and 'kE' in ttype: continue
+				detcondition = jets.replace('.fE', '.fDet=={}'.format(fdet))
+				sdet = 'EMCAL'
+				if fdet > 0: sdet = 'DCAL'
+				print '    ',sdet,detcondition,ttype,jets
+				h = get_jets(dname, jets, '(trig.type & {})&&({})'.format(bit, detcondition))
+				h.obj.Scale(1./get_n_events(dname, bit))
+				ls.add_to_list(sdet+' '+jets, h, '{} {}'.format(ttype, sdet), 'hist')
 
 	ls.draw_all(logy=True)
 	ls.pdf()
-	#ls.write_all(mod='modn:')
+	ls.write_all(mod='modn:')
+
+def draw():
+	files = [
+		'EMCAL_jets04.fE.root',
+		'EMCAL_jets02.fE.root',
+		'EMCAL_jets00.fE.root',
+		'DCAL_jets04.fE.root',
+		'DCAL_jets02.fE.root',
+		'DCAL_jets00.fE.root' ]
+
+	ls = dlist.ListStorage('jets_draw')
+	lsr = dlist.ListStorage('jets_draw_ratios')
+
+	for fn in files:
+		hl = dlist.dlist(fn)
+		hl.add_from_file('o_0', fn, 'kCINT7', 	'hist')
+		hl.add_from_file('o_1', fn, 'JE', 		'hist')
+		hl.add_from_file('o_2', fn, 'GA', 		'hist')
+		hl.rebin(2)
+		hl.scale(1./2.)
+		ls.append(hl)
+
+		hlr = hl.ratio_to(0, 'hist')
+		hlr.title = hl.title.split(' ')[0].replace('.root', '')
+		lsr.append(hlr)
+
+	ls.draw_all(logy=True)
+	lsr.draw_all(logy=True, maxy=2.e3)
 
 if __name__ == '__main__':
 	tu.setup_basic_root()
@@ -47,7 +102,10 @@ if __name__ == '__main__':
 		ROOT.gSystem.Load(lib)
 	dname = ut.get_arg_with('--in')
 	if dname==None:
-		dname = './'
-	main(dname)
+		dname = './245146'
+	if '--make' in sys.argv:
+		main(dname)
+	if '--draw' in sys.argv:
+		draw()
 	if not ut.is_arg_set('-b'):
 		IPython.embed()	
