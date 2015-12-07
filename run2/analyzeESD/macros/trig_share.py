9041cf7510a4ecd499ae251356d83d9f3f58a6d4
matplo <ploskon@gmail.com>
2015-12-07 20:48:55 +0100

    plots with V0M centrality

--- run2/analyzeESD/macros/trig_share.py
+++ run2/analyzeESD/macros/trig_share.py
@@ -80,6 +80,12 @@ def main(dname):
 
 	hl = dlist.dlist('t_share')
 
+	hlcent = dlist.dlist('t_share_cent')
+
+	cent = []
+	for i in range(0, 11):
+		cent.append([i*10, (i+1)*10])
+
 	nMB = 0.0
 	nevents = []
 	for i, tc in enumerate(tcond):
@@ -90,6 +96,24 @@ def main(dname):
 			nMB = n * 1.
 		h.SetBinContent(i+1, n / nMB)
 		hl.add(h, '[{}] {}'.format(i, htitle[i]), '+f1001 +a75 +l1 hist')
+
+		hn  = hname[i] + '_cent'
+		h  = ROOT.TH1F(hn, hn, 11, 0, 110)
+		for ic, c in enumerate(cent):
+			tcc = tc + ' && (trig.cent >= {} && trig.cent < {})'.format(c[0],c[1])
+			n   = get_n_events(dname, tcc)
+			h.SetBinContent(ic+1, n / nMB)
+		hlcent.add(h, htitle[i], 'hist')
+
+	hlcent.reset_axis_titles('centrality V0M %', 'ratio to N of kCINT7 triggers')
+
+	hlcent.make_canvas()
+	hlcent.draw(logy=True)
+	hlcent.self_legend()
+	hlcent.update()
+	hlcent.write_to_file(name_mod='modn:')
+	hlcent.pdf()
+	tu.gList.append(hlcent)
 
 	for i, tc in enumerate(tcond):
 		print tc, nevents[i]
