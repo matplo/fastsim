4540c417f94959d2dcc11803644567cfdf7e158d
matplo <ploskon@gmail.com>
2015-12-13 04:26:18 +0100

    example

---
+++ python/alice/get_lhc11h.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+
+#alien_find /alice/data/2011/LHC11h_2/000170207/ESDs/pass2 root_archive.zip | tee file_list.
+rnumber=000170207
+#alien_find /alice/data/2011/LHC11h/$rnumber/raw raw.roow | tee file_list_$rnumber.raw
+#alien_ls /alice/data/2011/LHC11h/$rnumber/raw | grep .root | tee "file_list_$rnumber_raw.txt"
+
+./get_raw.py -n 10 -r 000170207
+
+# this is for LHC15o
+./get_raw.py -n 10 -r 000246751
+
