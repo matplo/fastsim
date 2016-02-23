2c6ec22ba47d03dc54e5c9c6609ea523d85c0068
matplo <ploskon@gmail.com>
2016-02-24 00:32:43 +0100

    isolated wrapper (no dependencies)

--- fastsim/scripts/build_fastsim.sh
+++ fastsim/scripts/build_fastsim.sh
@@ -30,7 +30,7 @@ if [ -d $FASTSIM ]; then
   fi
 
 #  for pack in AliGenFME/src src
-  for pack in src
+  for pack in wrapper/src src
   do
     bdir=$FASTSIM/.build/$pack
     if is_arg_set "realclean" ;
---
+++ fastsim/wrapper/src/main.cxx
@@ -0,0 +1,8 @@
+#include "wrapper.h"
+
+int main ( int argc, char *argv[] )
+{
+	test_wrapper();
+	return 0;
+};
+
