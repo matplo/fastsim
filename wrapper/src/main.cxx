1cb1cba2bb7fa6f5782df3218c7c3dbd464b09ac
matplo <ploskon@gmail.com>
2016-02-24 02:29:30 +0100

    cleanup & optimization; removed get(T **p) - not needed; added add(const T &) -> calls new (!) internally and stores the pointer; tried for std::vector - works fine (see main.cxx); added ::list() to print things into the screen

--- wrapper/src/main.cxx
+++ wrapper/src/main.cxx
@@ -1,23 +1,51 @@
 #include "wrapper.h"
 #include <iostream>
 
+void other_function(Wrapper &wr)
+{
+	std::vector<int> *vp = wr.get< std::vector<int> >();
+	std::vector<int> &avp = *vp;
+	std::cout << " value: " << avp[1] << " (other_function) " << std::endl;
+}
+
 void test_wrapper()
 {
-	Wrapper wr;
+	std::cout << "[i] testing wrapper..." << std::endl;
 
 	WrapTestClass *tc = new WrapTestClass();
+	std::cout << "   tc  at " << tc  << std::endl;
+
+	Wrapper wr;
 	wr.add(tc);
 
-	WrapTestClass *tc1 = 0;
-	tc1 = wr.get(tc1);
+	WrapTestClass *tc1 = wr.get<WrapTestClass>();
+	std::cout << "   tc1 at " << tc1 << std::endl;
 
-	WrapTestClass *tc2 = 0;
-	wr.get(&tc2);
+	int *t = wr.get<int>();
+	std::cout << "   int t at " << t << std::endl;
 
-	//cout << "tc  at " << tc  << endl;
-	//cout << "tc1 at " << tc1 << endl;
-	//cout << "tc2 at " << tc2 << endl;
+	std::vector<int> v;
+	v.push_back(123);
+	v.push_back(345);
+	wr.add(v);
 
+	std::vector<int> *vp = wr.get< std::vector<int> >();
+	std::vector<int> &avp = *vp;
+
+	std::cout << " vector size: " << avp.size() 
+				<<	" should be: " << v.size()
+				<< std::endl;
+
+	std::cout << " value: " << avp[0] << std::endl;
+	other_function(wr);
+
+	// obsolete - not needed implementation
+	//WrapTestClass *tc2 = 0;
+	//wr.get(&tc2);
+	//std::cout << "   tc2 at " << tc2 << std::endl;
+
+	std::cout << std::endl;
+	wr.list();
 }
 
 int main ( int argc, char *argv[] )
