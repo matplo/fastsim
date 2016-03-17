5efb92e7537bdb59b0bb086dd449a349d7e73b0d
matplo <ploskon@gmail.com>
2016-03-17 01:10:43 +0100

    fixed also default cmnd file; CR and no CR (w/o CR default settings)

---
+++ test/run_test_cr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+module list
+module load use.own
+module load hepsoft/latest
+module load fastsim
+module list
+
+cd $FASTSIM/test/
+nev=$1
+[ -z $nev ] && nev=10000
+# the code below makes a 10 GeV uubar-qark events
+#time fastsim -nev $nev -pgunid 2 -pgunpt 10.
+#time fastsim -nev $nev -pgunid 21 -pgunpt 2. -pgunptmax 100.0
+#for q in 1 2 3 4 5 6 21
+#do
+#	time fastsim -nev $nev -pgunid $q -pgunpt 1.0 -pgunptmax 100.0 -out parton_$q.root
+#done
+
+# this is using the .cmnd file
+time fastsim -nev $nev -config ../config/pythia_multiple_coll_cr.cmnd -out cr.root
+time fastsim -nev $nev -config ../config/pythia_multiple_coll_NOcr.cmnd -out NOcr.root
